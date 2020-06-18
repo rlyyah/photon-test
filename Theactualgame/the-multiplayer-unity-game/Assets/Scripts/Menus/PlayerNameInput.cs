15c6aee47671144e7b405729a4d4f41d7486e836
rlyyah <39671388+rlyyah@users.noreply.github.com>
2020-06-18 19:57:29 +0200

    Add Menu Propotoype with playername and simple connection script

---
+++ Theactualgame/the-multiplayer-unity-game/Assets/Scripts/Menus/PlayerNameInput.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameInput : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameInputField = null;
+    [SerializeField] private Button continueButton = null;
+
+    private const string PlayerPrefsNamesKey = "PlayerName";
+
+    private void Start() => SetUpInputField();
+
+    private void SetUpInputField()
+    {
+        if(!PlayerPrefs.HasKey(PlayerPrefsNamesKey)) { return; }
+
+        string defaultName = PlayerPrefs.GetString(PlayerPrefsNamesKey);
+
+        nameInputField.text = defaultName;
+
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+
+        //continueButton.interactable = !string.IsNullOrEmpty(name);
+        continueButton.interactable = !string.IsNullOrEmpty(nameInputField.text);
+    }
+     
+    public void SavePlayerName()
+    {
+        string playerName = nameInputField.text;
+
+        PhotonNetwork.NickName = playerName;
+
+        PlayerPrefs.SetString(PlayerPrefsNamesKey, playerName);
+    }
+}
